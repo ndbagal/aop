186d871c8c94baafd891bd872a4b0a9dcef36327
Navnath Bagal <navnathbagal@live.in>
2026-03-15 08:59:44 +0530

    feat(auth): integrate better-auth for authentication and user management
    - Added better-auth package for handling authentication.
    - Updated User model in Prisma schema to include fields for email verification and user sessions.
    - Created new Session, Account, and Verification models in Prisma schema.
    - Implemented protected routes for user authentication in TRPC.
    - Created login and signup pages with forms for user input.
    - Added auth layout for consistent styling across authentication pages.
    - Implemented social login functionality for GitHub and Google.
    - Created utility functions for requiring authentication and unauthenticated access.
    - Updated API routes to handle authentication requests.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
 const Home = async () => {
-  const queryClient = getQueryClient();
+  await requireAuth();
 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  const data = await caller.getUsers();
 
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      Protected server page. You should only see this if you are authenticated.
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
     </div>
   );
 };
